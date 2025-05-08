8ee178d89b986bdb03fc2010befa42fbe2d7a0f1
PhytoIn <136543606+PhytoIn@users.noreply.github.com>
2025-05-08 17:43:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -343,6 +343,6 @@ if buscar_nomes:
             st.write("**Nenhuma correspondência encontrada**")
 
        except Exception as e:
-        st.error(f"Erro durante o processamento: {str(e)}")
+            st.error(f"Erro durante o processamento: {str(e)}")
 elif uploaded_file is not None and not candidates_input:
-    st.warning("Por favor, insira os nomes dos candidatos para comparação.")
+       st.warning("Por favor, insira os nomes dos candidatos para comparação.")
